f03e7bd3e9617d2186c80c71ea0caa746399f27c
Andrey Patotski <7906241@gmail.com>
2021-08-14 18:12:38 +0300

    connect chatDB for ChatFragment.kt

--- app/src/main/java/com/progerchat/achat/screens/chat/ChatFragment.kt
+++ app/src/main/java/com/progerchat/achat/screens/chat/ChatFragment.kt
@@ -2,20 +2,28 @@ package com.progerchat.achat.screens.chat
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
-import com.progerchat.achat.R
+import com.progerchat.achat.database.messages.MessageDB
+import com.progerchat.achat.database.messages.MessageRepository
 import com.progerchat.achat.databinding.ChatFragmentBinding
-import com.progerchat.achat.databinding.LoginFragmentBinding
-import com.progerchat.achat.screens.login.LoginFragmentDirections
-import com.progerchat.achat.screens.login.LoginViewModel
+import com.progerchat.achat.model.Message
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class ChatFragment : Fragment() {
+    private val applicationScope = CoroutineScope(SupervisorJob())
+    private val database by lazy { MessageDB.getInstance(requireActivity(), applicationScope) }
+    private val repository by lazy { MessageRepository(database.messageDAO()) }
 
-    private lateinit var viewModel: ChatViewModel
+    private var _viewModel: ChatViewModel? = null
+    private val viewModel get() = _viewModel
+
     private var _binding: ChatFragmentBinding? = null
     private val binding get() = _binding!!
 
@@ -24,17 +32,20 @@ class ChatFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = ChatFragmentBinding.inflate(inflater, container, false)
-        viewModel = ViewModelProvider(this).get(ChatViewModel::class.java)
+        val application = requireNotNull(this).activity?.application
+        val viewModelFactory = application?.let { ChatFactory(repository, it) }
+        _viewModel =
+            viewModelFactory?.let { ViewModelProvider(this, it) }?.get(ChatViewModel::class.java)
 
+        viewModel?.elements?.observe(viewLifecycleOwner, Observer {
+            Log.i("MyLog",it.toString())
+        })
 
         binding.apply {
-            //переход на другую активити
             button.setOnClickListener {
-                view?.findNavController()
-                    ?.navigate(ChatFragmentDirections.actionChatFragmentToLoginFragment())
+                viewModel?.insert(Message())
             }
         }
-
 
         return binding.root
     }
