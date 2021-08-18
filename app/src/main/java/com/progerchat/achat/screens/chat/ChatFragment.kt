2de11eb4d02d83ee30962836a42d062b966d9905
Bahdan Dzioba <dziobadzz@gmail.com>
2021-08-18 23:14:56 +0300

    fix: add list contact

--- app/src/main/java/com/progerchat/achat/screens/chat/ChatFragment.kt
+++ app/src/main/java/com/progerchat/achat/screens/chat/ChatFragment.kt
@@ -6,10 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.progerchat.achat.R
 import com.progerchat.achat.databinding.ChatFragmentBinding
+import com.progerchat.achat.databinding.LoginFragmentBinding
+import com.progerchat.achat.screens.contacts.Contact
+import com.progerchat.achat.screens.login.LoginFragmentDirections
+import com.progerchat.achat.screens.login.LoginViewModel
 import com.progerchat.achat.utils.OnListClickListener
 
 class ChatFragment : Fragment(), OnListClickListener {
@@ -25,14 +31,15 @@ class ChatFragment : Fragment(), OnListClickListener {
     ): View {
         _binding = ChatFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this).get(ChatViewModel::class.java)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        adapter = viewModel.contacts?.value?.let {
-            ChatAdapter(this, it.toMutableList())
-        }
-
-        binding.list.adapter = adapter
-
+        // add adapter
+        val recyclerView = view.findViewById<RecyclerView>(R.id.list)
+        recyclerView.adapter = viewModel.contacts?.value?.let { ChatAdapter(this, it.toMutableList()) }
 
         val swipeToDeleteCallBack = object : SwipeChat() {
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
@@ -42,11 +49,16 @@ class ChatFragment : Fragment(), OnListClickListener {
             }
         }
         val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallBack)
-        itemTouchHelper.attachToRecyclerView(binding.list)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
 
-        return binding.root
+        viewModel.contacts?.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter?.notifyDataSetChanged()
+                // adapter?.addItems(it)
+            }
+        })
+
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
--- app/src/main/java/com/progerchat/achat/screens/chat/ChatViewModel.kt
+++ app/src/main/java/com/progerchat/achat/screens/chat/ChatViewModel.kt
@@ -3,7 +3,7 @@ package com.progerchat.achat.screens.chat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.progerchat.achat.model.Contact
+import com.progerchat.achat.screens.contacts.Contact
 
 class ChatViewModel : ViewModel() {
 
